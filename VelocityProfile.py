54dfd986f10f2ac77f4cd38a951baf634bcfbf32
mlen-michelle <49734387+mlen-michelle@users.noreply.github.com>
2019-04-17 15:58:51 -0700

    Updated spaces to tabs
    Updated all spaces to tabs

--- VelocityProfile.py
+++ VelocityProfile.py
@@ -3,35 +3,35 @@ from VelocityProfileParser import VelocityProfileParser
 import sys
 
 def velocity_data(filename):
-    all_data = VelocityProfileParser(filename).all_steps
-    all_velocity_details = {}
+	all_data = VelocityProfileParser(filename).all_steps
+	all_velocity_details = {}
 
-    graph_data = []
-    for timestep in all_data:
-        timestep_details = all_data[timestep]
-        all_velocity_details[timestep] = []
-        coord = []
-        speed = []
-        for data in timestep_details:
-            coord.append(float(data[1]))
-            speed.append(float(data[3]))
-            step_details = [coord, speed]
-            all_velocity_details[timestep] = step_details
-        # slope, intercept, r_value, p_value, std_err = stats.linregress(coord[1:9], speed[1:9])
-        slope, intercept, r_value, p_value, std_err = stats.linregress(coord[1:-1], speed[1:-1])
+	graph_data = []
+	for timestep in all_data:
+		timestep_details = all_data[timestep]
+		all_velocity_details[timestep] = []
+		coord = []
+		speed = []
+		for data in timestep_details:
+			coord.append(float(data[1]))
+			speed.append(float(data[3]))
+			step_details = [coord, speed]
+			all_velocity_details[timestep] = step_details
+		# slope, intercept, r_value, p_value, std_err = stats.linregress(coord[1:9], speed[1:9])
+		slope, intercept, r_value, p_value, std_err = stats.linregress(coord[1:-1], speed[1:-1])
 
-        graph_datum = {
-            'time': timestep,
-            'r2': r_value**2,
-            'shear': slope
-        }
+		graph_datum = {
+			'time': timestep,
+			'r2': r_value**2,
+			'shear': slope
+		}
 
-        graph_data.append(graph_datum)
+		graph_data.append(graph_datum)
 
-    return graph_data
+	return graph_data
 
 if len(sys.argv) < 2:
-    sys.exit('please pass a filename as first argument')
+	sys.exit('please pass a filename as first argument')
 graph_data = velocity_data(sys.argv[1])
 
 new_file = open('R-Squared Value', 'w')
@@ -39,10 +39,10 @@ new_file.write('Timestep \t R-Squared \t Slope \n')
 sorted_graph_data = sorted(graph_data, key=lambda x: x['time'])
 
 for data in sorted_graph_data:
-    a = data['time']
-    b = data['r2']
-    c = data['shear']
-    new_file.write(str(a) + '\t' + str(b) + '\t' + str(c) + '\n')
+	a = data['time']
+	b = data['r2']
+	c = data['shear']
+	new_file.write(str(a) + '\t' + str(b) + '\t' + str(c) + '\n')
 
 new_file.close()
 
